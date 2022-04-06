c03d7a5c13c4eec46b483aeda8c15bda3ed7b955
apsa <n.Lohrer@campus.lmu.de>
2022-04-06 21:24:58 +0200

    Add examples

--- examples.py
+++ examples.py
@@ -1,4 +1,8 @@
 import synth
 
 synth.sin_to_file(220, sec = 10, fname = "concert_a3.wav")
-synth.sin_to_file(440, sec = 10, fname = "concaert_a4.wav")
+synth.sin_to_file(440, sec = 10, fname = "concert_a4.wav")
+synth.sin_to_file(440 * 2**(1 / 12), sec = 10, fname = "concert_bb4.wav")
+synth.sin_to_file(440 * 2**(2 / 12), sec = 10, fname = "concert_b4.wav")
+synth.sin_to_file(440 * 2**(7 / 12), sec = 10, fname = "concert_e5.wav")
+synth.sin_to_file(440 * 2**(1 / 24), sec = 10, fname = "half_sharp_a4.wav")
