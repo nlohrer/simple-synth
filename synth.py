e9f548180ec1a4aa7c1b72a27253f9b5a5188194
apsa <n.Lohrer@campus.lmu.de>
2022-04-06 20:43:55 +0200

    Add function to create sine oscillator

--- synth.py
+++ synth.py
@@ -1,3 +1,5 @@
+import numpy
+from scipy.io import wavfile
 import math
 import itertools
 
@@ -8,5 +10,16 @@ def get_sin_oscillator(frequency, sample_rate, amplitude):
 
 osc = get_sin_oscillator(frequency = 1, sample_rate = 44100, amplitude = 1)
 samples = [next(osc) for i in range(512)]
+
+def wave_to_file(wav, wav2 = None, fname = "temp.wav", amp = 0.1, sample_rate = 44100):
+  wav = numpy.array(wav)
+  wav = nympy.int16(wav * amp * (2**15 - 1))
+
+  if wav2 is not None:
+    wav2 = nympy.array(wav2)
+    wav2 = np.int16(wav2 * amp * (2 ** 15 - 1))
+    wav = np.stack([wav, wav2]).T
+
+  wavfile.write(fname, sample_rate, wav)
 
 print(samples)
