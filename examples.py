31295be1430b80e031df0c2419f1cbc9b51f9d0f
apsa <n.Lohrer@campus.lmu.de>
2022-04-06 21:10:48 +0200

    Implement sine synth

---
+++ examples.py
@@ -0,0 +1,4 @@
+import synth
+
+synth.sin_to_file(220, sec = 10, fname = "concert_a3.wav")
+synth.sin_to_file(440, sec = 10, fname = "concaert_a4.wav")
--- synth.py
+++ synth.py
@@ -4,22 +4,16 @@ import math
 import itertools
 
 
-def get_sin_oscillator(frequency, sample_rate, amplitude):
+def get_sin_oscillator(frequency, amplitude, sample_rate = 44100):
   increment = (2 * math.pi * frequency) / sample_rate
   return (math.sin(v) * amplitude for v in itertools.count(start = 0, step = increment))
 
-osc = get_sin_oscillator(frequency = 1, sample_rate = 44100, amplitude = 1)
-samples = [next(osc) for i in range(512)]
-
-def wave_to_file(wav, wav2 = None, fname = "temp.wav", amp = 0.1, sample_rate = 44100):
+def wave_to_file(wav, fname = "temp.wav", amp = 0.1, sample_rate = 44100):
   wav = numpy.array(wav)
-  wav = nympy.int16(wav * amp * (2**15 - 1))
-
-  if wav2 is not None:
-    wav2 = nympy.array(wav2)
-    wav2 = np.int16(wav2 * amp * (2 ** 15 - 1))
-    wav = np.stack([wav, wav2]).T
-
+  wav = numpy.int16(wav * amp * (2**15 - 1))
   wavfile.write(fname, sample_rate, wav)
 
-print(samples)
+def sin_to_file(freq, sec, fname = "temp.wav", amp = 0.1, sample_rate = 44100):
+  osc = get_sin_oscillator(frequency = freq, amplitude = 1)
+  samples = [next(osc) for i in range(44100 * sec)]
+  wave_to_file(samples, fname = fname, amp = amp, sample_rate = sample_rate)
