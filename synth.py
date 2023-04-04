381b29bd06ae837ddb494694327c27207625efc8
apsa <n.Lohrer@campus.lmu.de>
2023-04-05 01:36:50 +0200

    Add function for simple additive synthesis

--- synth.py
+++ synth.py
@@ -3,17 +3,28 @@ from scipy.io import wavfile
 import math
 import itertools
 
-
 def get_sin_oscillator(frequency, amplitude, sample_rate = 44100):
-  increment = (2 * math.pi * frequency) / sample_rate
-  return (math.sin(v) * amplitude for v in itertools.count(start = 0, step = increment))
+    increment = (2 * math.pi * frequency) / sample_rate
+    return (math.sin(v) * amplitude for v in itertools.count(start = 0, step = increment))
 
-def wave_to_file(wav, fname = "temp.wav", amp = 0.1, sample_rate = 44100):
-  wav = numpy.array(wav)
-  wav = numpy.int16(wav * amp * (2**15 - 1))
-  wavfile.write(fname, sample_rate, wav)
+def sin_to_wav(freq, sec, amp = 0.1, sample_rate = 44100):
+    osc = get_sin_oscillator(frequency = freq, amplitude = 1)
+    samples = [next(osc) for i in range(44100 * sec)]
+    wav = numpy.array(samples)
+    wav = numpy.int16(wav * amp * (2**15 - 1))
+    return wav
+    
+def sin_to_file_additive(freqs, amps, sec, fname = 'temp.wav', sample_rate = 44100):
+    wavs = []
+    for freq, amplitude in zip(freqs, amps):
+        wav = sin_to_wav(freq, sec, amplitude, sample_rate)
+        wavs.append(wav)
+        wavfile.write(str(freq) + '.wav', sample_rate, wav)
+    combined_wav = wavs[0]
+    for wav in wavs[1:]:
+        combined_wav += wav
+    wavfile.write(fname, sample_rate, combined_wav)
 
 def sin_to_file(freq, sec, fname = "temp.wav", amp = 0.1, sample_rate = 44100):
-  osc = get_sin_oscillator(frequency = freq, amplitude = 1)
-  samples = [next(osc) for i in range(44100 * sec)]
-  wave_to_file(samples, fname = fname, amp = amp, sample_rate = sample_rate)
+    wav = sin_to_wav(freq, sec, amp, sample_rate)
+    wavfile.write(fname, sample_rate, wav)
