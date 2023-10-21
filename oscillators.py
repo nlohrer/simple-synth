e83cdd42aea2e0421b9f939cedffe27491ff1ec0
apsa <n.Lohrer@campus.lmu.de>
2023-10-21 15:50:49 +0200

    Split up oscillator functions for easier wave manipulation

--- oscillators.py
+++ oscillators.py
@@ -20,8 +20,16 @@ def get_sin_oscillator(frequency, amplitude, sample_rate = 44100):
     increment = (2 * math.pi * frequency) / sample_rate
     return (wave.math.sin(v) * amplitude for v in itertools.count(start = 0, step = increment))
 
-def osc_to_wav(osc, sec, amp = 0.1, sample_rate = 44100):
+def osc_to_array(osc, sec, sample_rate = 44100):
     samples = [next(osc) for i in range(round(44100 * sec))]
-    wav = numpy.array(samples)
-    wav = numpy.int16(wav * amp * (2**15 - 1))
+    wav_array = numpy.array(samples)
+    return wav_array
+
+def array_to_wav(wav_array, amp):
+    wav = numpy.int16(wav_array * amp * (2**15 - 1))
+    return wav
+
+def osc_to_wav(osc, sec, amp = 0.1, sample_rate = 44100):
+    wav_array = osc_to_array(osc, sec, sample_rate)
+    wav = array_to_wav(wav_array, amp)
     return wav
