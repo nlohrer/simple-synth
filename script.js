c71dfce3d56d619dac56e85a9cab58fc72d2a373
apsa <n.Lohrer@campus.lmu.de>
2023-10-22 19:50:17 +0200

    Add envelope option to frontent

--- script.js
+++ script.js
@@ -2,20 +2,37 @@ url = "http://localhost:6500";
 const head = document.querySelector('#head');
 const submit = document.querySelector("#submit");
 var id = 1;
-
 submit.addEventListener("click", addWAVs)
 
+const frequencyField = document.querySelector("#frequency");
+const secondsField = document.querySelector("#sec");
+const ampField = document.querySelector("#amp");
+const waveformField = document.querySelector("#waveform");
+const attackField = document.querySelector("#attack");
+const decayField = document.querySelector("#decay");
+const releaseField = document.querySelector("#release");
+const attackLabel = document.querySelector("#attack-label");
+const decayLabel = document.querySelector("#decay-label");
+const releaseLabel = document.querySelector("#release-label");
+
+envelopeFields = [secondsField, attackField, decayField, releaseField];
+keepEnvelopeBalance();
+envelopeFields.forEach((field) => {
+    field.addEventListener("input", keepEnvelopeBalance);
+});
+
+
 async function addWAVs() {
-    const frequencyField = document.querySelector("#frequency");
-    const secondsField = document.querySelector("#sec");
-    const ampField = document.querySelector("#amp");
-    const waveformField = document.querySelector("#waveform");
     const frequency = frequencyField.value.trim();
     const seconds = secondsField.value.trim();
     const amplitude = ampField.value.trim();
     const waveform = waveformField.value;
+    const attack = attackField.value;
+    const decay = decayField.value;
+    const release = releaseField.value;
+    const envelope = `{"attack": ${attack}, "decay": ${decay}, "release": ${release}}`
 
-    const wavURL = await createWAV(frequency, seconds, amplitude, waveform);
+    const wavURL = await createWAV(frequency, seconds, amplitude, waveform, envelope);
     addWAVToContainer(wavURL);
     console.log(wavURL);
 }
@@ -34,14 +51,8 @@ function addWAVToContainer(url) {
     audio.appendChild(source);
 }
 
-async function getInfo() {
-    response = await fetch(url);
-    response_text = await response.text()
-    console.log(response_text);
-}
-
-async function createWAV(frequency, duration, amplitude, waveform) {
-    body = `{"frequency": ${frequency}, "seconds": ${duration}, "amplitude": ${amplitude}, "waveform": "${waveform}"}`;
+async function createWAV(frequency, duration, amplitude, waveform, envelope) {
+    body = `{"frequency": ${frequency}, "seconds": ${duration}, "amplitude": ${amplitude}, "waveform": "${waveform}", "envelope": ${envelope}}`;
     response = await fetch(`${url}/synth/${id++}`, {
         method: 'POST',
         headers: {
@@ -52,4 +63,34 @@ async function createWAV(frequency, duration, amplitude, waveform) {
 
     response_text = await response.text();
     return response_text;
+}
+
+async function getInfo() {
+    response = await fetch(url);
+    response_text = await response.text()
+    console.log(response_text);
+}
+
+function keepEnvelopeBalance() {
+    const seconds = secondsField.value.trim();
+    const attack = attackField.value;
+    const decay = decayField.value;
+    const release = releaseField.value;
+
+    const attackRemainder = seconds - decay - release;
+    const decayRemainder = seconds - attack - release;
+    const releaseRemainder = seconds - attack - decay;
+
+    attackField.max = attackRemainder > 0 ? attackRemainder : 0;
+    decayField.max = decayRemainder ? decayRemainder : 0;
+    releaseField.max = releaseRemainder ? releaseRemainder : 0;
+
+    attackLabel.textContent = `Attack: ${parseFloat(attack).toFixed(2)}`;
+    decayLabel.textContent = `Decay: ${parseFloat(decay).toFixed(2)}`;
+    releaseLabel.textContent = `Release: ${parseFloat(release).toFixed(2)}`;
+
+    console.log("---------------")
+    console.log("attack max:" + attackField.max);
+    console.log("decay max:" + decayField.max);
+    console.log("release max:" + releaseField.max);
 }
